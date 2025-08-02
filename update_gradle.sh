5588f028535560fa502f790b9ae8d65351878deb
abhay-byte <abhay02delhi@gmail.com>
2025-08-02 17:30:25 +0530

    Update the update_gradle.sh file

--- update_gradle.sh
+++ update_gradle.sh
@@ -45,6 +45,10 @@ cat <<EOL > android/app/proguard-rules.prop
 # Retain generic signatures of TypeToken and its subclasses with R8 version 3.0 and higher.
 -keep,allowobfuscation,allowshrinking class com.google.gson.reflect.TypeToken
 -keep,allowobfuscation,allowshrinking class * extends com.google.gson.reflect.TypeToken
+
+# Missing javax.annotation classes - suppress warnings
+-dontwarn javax.annotation.Nullable
+-dontwarn javax.annotation.concurrent.GuardedBy
 EOL
 
 echo "Changes applied."
@@ -54,4 +58,4 @@ git update-index --assume-unchanged android/settings.gradle
 
 echo "Files marked as unchanged in git."
 echo "Press any key to continue..."
-read -n 1 -s
+read -k 1
