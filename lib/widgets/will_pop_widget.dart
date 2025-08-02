d06528c04d09d95774ba00136dee538e07803884
abhay-byte <abhay02delhi@gmail.com>
2025-08-02 17:09:02 +0530

    98: Change back to WillPopScope until it is fully removed

--- lib/widgets/will_pop_widget.dart
+++ lib/widgets/will_pop_widget.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
 
-/// A widget that registers a callback to veto attempts by the user to dismiss
-/// the enclosing [ModalRoute].
-///
-/// This widget is similar in behavior to the deprecated `WillPopScope` widget,
-/// but implemented using the new [PopScope] widget. It takes the same arguments
-/// as `WillPopScope`.
-///
-/// When a pop is attempted, if [onWillPop] is null, the pop is allowed.
-/// Otherwise, [onWillPop] is called. If it returns a [Future] that resolves
-/// to `true`, the pop is allowed. If it resolves to `false`, the pop is
-/// prevented.
 class WillPopWidget extends StatefulWidget {
-  /// Creates a widget that registers a callback to veto attempts by the user to
-  /// dismiss the enclosing [ModalRoute].
   const WillPopWidget({
     super.key,
     required this.child,
     required this.onWillPop,
   });
 
-  /// The widget below this widget in the tree.
   final Widget child;
 
-  /// Called to veto attempts by the user to dismiss the enclosing [ModalRoute].
-  ///
-  /// If the callback returns a [Future] that resolves to `false`, the enclosing
-  /// route will not be popped. Otherwise, the route will be popped.
-  ///
-  /// If the callback is null, the enclosing route will be popped unconditionally.
   final Future<bool> Function()? onWillPop;
 
   @override
@@ -41,15 +21,12 @@ class _WillPopWidgetState extends State<WillPopWidget> {
     final onWillPopCallback = widget.onWillPop;
 
     if (onWillPopCallback == null) {
-      // If onWillPop is null, allow popping unconditionally by setting canPop to true.
       return PopScope(
         canPop: true,
         child: widget.child,
       );
     }
 
-    // If onWillPop is provided, we initially set canPop to false.
-    // The decision to pop is then handled asynchronously in onPopInvoked.
     return WillPopScope(
       onWillPop: onWillPopCallback,
       child: widget.child,
